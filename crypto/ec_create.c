604439012c97ee64248b5b18b4ab23ae3303ffbd
Loris Leveque <9601@holbertonstudents.com>
2025-03-07 10:38:06 +0100

    Pushed last changes

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -30,9 +30,11 @@ EC_KEY *ec_create(void)
 	/* create a _struct_ based on EC_CURVE (elliptic) */
 	key = EC_KEY_new_by_curve_name(EC_CURVE); /* see: NID_secp256k1 */
 	if (!key) /* uh oh */
+	{
 		return (NULL);
 		fprintf(stderr, "Failure " __LINE__ ":" __func__
 				": generating EC_KEY");
+	}
 	/* generate the actual key, return 1 on success */
 	if (!EC_KEY_generate_key(key))
 	{
