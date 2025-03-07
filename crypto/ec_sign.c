ffb90bed465e6c149df2ce56c368eb7150146ea0
Loris Leveque <9601@holbertonstudents.com>
2025-03-07 10:26:43 +0100

    Pushed last changes

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -25,7 +25,7 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	if (!key || !msg || !sig)
 		return (NULL);
 
-	if (ECDSA_sign(0, msg, msglen, sig->sig, &sig->len, (EC_KEY *)key) != 1)
+	if (ECDSA_sign(0, msg, msglen, sig->sig, &(sig->len), (EC_KEY *)key) != 1)
 		return (NULL);
 
 	return (sig->sig);
