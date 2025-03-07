3e9b3ec48640c387b06a639432123b2416153f78
Loris Leveque <9601@holbertonstudents.com>
2025-03-07 10:43:01 +0100

    Pushed last changes

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -30,7 +30,7 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 		sig_t *sig)
 {
-	unsigned int sig_len = SIG_MAX_LENGTH;
+	unsigned int sig_len = SIG_MAX_LEN;
 
 	if (!key || !msg || !sig)
 		return (NULL);
