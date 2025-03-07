ca65ea154c19b7526b9be7f351855b9a52055528
Loris Leveque <9601@holbertonstudents.com>
2025-03-07 10:35:12 +0100

    Pushed last changes

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -32,7 +32,7 @@ int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 		sig_t const *sig)
 {
 	if (!key || !msg || !sig)
-		return (NULL);
+		return (0);
 
 	return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len,
 				(EC_KEY *)key) == 1);
