9d6dfcad67ffd3a74549041aa33796dfd0b236a5
El-Chapo0133 <levequeloris66@gmail.com>
2025-03-14 15:59:09 +0100

    Pushed last changes

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -39,7 +39,7 @@ int blockchain_serialize(blockchain_t const *blockchain,
 	}
 	dup2(fd, STDOUT_FILENO);
 
-	(void *)blockchain;
+	(void)blockchain;
 	printf(HBLK_MAGIC VERSION);
 
 	dup2(save_fd, STDOUT_FILENO);
