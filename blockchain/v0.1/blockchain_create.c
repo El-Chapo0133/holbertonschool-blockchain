f45272a2d8ef01f4668d324fbed7d7d20958bdd2
El-Chapo0133 <levequeloris66@gmail.com>
2025-03-14 10:21:58 +0100

    Pushed last changes

---
+++ blockchain/v0.1/blockchain_create.c
@@ -0,0 +1,65 @@
+/*
+ * ==========================================================================
+ *
+ *       Filename:  blockchain_create.c
+ *
+ *    Description:  creates a Blockchain structure, and initializes it
+ *
+ *        Version:  1.0
+ *        Created:  14.03.2025 09:19:42
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Loris Levêque (9601), loris@leveque.website
+ *   Organization:  Holberton
+ *
+ * ==========================================================================
+ */
+
+#include "blockchain.h"
+
+/*
+ * blockchain_create - create a blockchain_t
+ */
+blockchain_t *blockchain_create(void)
+{
+	block_t *block;
+	blockchain_t *blockchain;
+	llist_t *list = llist_create(MT_SUPPORT_TRUE);
+	int index = 0;
+
+	blockchain = malloc(sizeof(blockchain_t));
+	block = malloc(sizeof(block_t));
+	if (!chain || !block)
+	{
+		fprintf(stderr, "cannot allocate for chain or block");
+		free(chain), free(block), llist_destroy(list, 1, NULL);
+		return (NULL);
+	}
+
+	/* meta data settings */
+	(block->metadata).index = GENESIS_INDEX;
+	(block->metadata).difficulty = GENESIS_DIFFICULTY;
+	(block->metadata).timestamp = GENESIS_TIMESTAMP;
+	(block->metadata).nonce = GENESIS_NONCE;
+	for (index = 0; index < SHA256_DIGEST_LENGTH; index++)
+		(block->metadata).prev_hash[index] = GENESIS_PREV_HASH;
+	/* data settings */
+	block->data.len = GENESIS_LEN;
+	memcpy(block->data.buffer, GENESIS_BUFFER, GENESIS_LEN);
+	/* hash set */
+	memcpy(block->hash, GENESIS_HASH, SHA256_DIGEST_LENGTH);
+
+	if (llist_add_node(list, block, ADD_NODE_FRONT))
+	{
+		fprintf(stderr, "Cannot add node in the llist");
+		free(chain), free(block), llist_destroy(list, 1, NULL);
+		return (NULL);
+	}
+
+	blockchain->chain = list;
+	return (chain);
+}
+
+
+
