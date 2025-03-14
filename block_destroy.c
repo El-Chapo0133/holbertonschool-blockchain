d208c5dbfa4d218480ae9a70d0bf1be562864747
El-Chapo0133 <levequeloris66@gmail.com>
2025-03-14 11:03:13 +0100

    Pushed last changes

---
+++ block_destroy.c
@@ -0,0 +1,28 @@
+/*
+ * ==========================================================================
+ *
+ *       Filename:  block_destroy.c
+ *
+ *    Description:  deletes an existing Block
+ *
+ *        Version:  1.0
+ *        Created:  14.03.2025 10:59:08
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Loris Levêque (9601), loris@leveque.website
+ *   Organization:  Holberton
+ *
+ * ==========================================================================
+ */
+
+#include "blockchain.h"
+
+/**
+ * block_destroy - destroy a given block
+ * @block: block to destroy
+ */
+void block_destroy(block_t *block)
+{
+	free(block);
+}
