a33aa742bf378b891120b5dcfa4725ba3c562b62
El-Chapo0133 <levequeloris66@gmail.com>
2025-03-28 11:31:56 +0100

    Pushed last changes

---
+++ blockchain/v0.2/block_mine.c
@@ -0,0 +1,29 @@
+/*
+ * ==========================================================================
+ *
+ *       Filename:  block_mine.c
+ *
+ *    Description:  mines a Block in order to insert it in the Blockchain
+ *
+ *        Version:  1.0
+ *        Created:  28.03.2025 10:22:48
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Loris Levêque (9601), loris@leveque.website
+ *   Organization:  Holberton
+ *
+ * ==========================================================================
+ */
+
+#include "blockchain.h"
+
+/**
+ * block_mine - mines a Block in order to insert it in the Blockchain
+ * @block: ptr to the block to be mined
+ */
+void block_mine(block_t *block)
+{
+	
+}
+
