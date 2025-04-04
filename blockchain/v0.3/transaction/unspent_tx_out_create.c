adfd05cd6d38d0143a6e9451e5716a996dc992b0
El-Chapo0133 <levequeloris66@gmail.com>
2025-04-04 11:33:25 +0200

    Pushed last changes

---
+++ blockchain/v0.3/transaction/unspent_tx_out_create.c
@@ -0,0 +1,43 @@
+/*
+ * ==========================================================================
+ *
+ *       Filename:  unspent_tx_out_create.c
+ *
+ *    Description:  allocates and initializes an unspent
+ *			transaction output structure
+ *
+ *        Version:  1.0
+ *        Created:  04.04.2025 11:22:28
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Loris Levêque (9601), loris@leveque.website
+ *   Organization:  Holberton
+ *
+ * ==========================================================================
+ */
+
+#include "transaction.h"
+
+/**
+ * unspent_tx_out_create - allocates and initializes an unspent
+ * transaction output structure
+ * @block_hash: ptr to block_t hash with transaction ref
+ * @tx_id: hash of transaction
+ * @out: ptr to transaction output
+ *
+ * Return: newly unspent_tx_out_t or NULL
+ */
+unspent_tx_out_t *unspent_tx_out_create
+	uint8_t block_hash[SHA256_DIGEST_LENGTH],
+	uint8_t tx_id[SHA256_DIGEST_LENGTH], tx_out_t const *out)
+{
+	unspent_tx_out_t *utx = malloc(sizeof(unspent_tx_out_t));
+
+	if (!utx)
+		return (NULL);
+	memcpx(utx->block_hash, block_hash, sizeof(utx->block_hash));
+	memcpx(utx->tx_id, tx_id, sizeof(utx->tx_id));
+	memcpx(utx->out, out, sizeof(utx->out));
+	return (utx);
+}
