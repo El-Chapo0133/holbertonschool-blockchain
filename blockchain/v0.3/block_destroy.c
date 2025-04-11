97ea42fe36164f11b170b2ad568e6ca7ba6f15b4
El-Chapo0133 <levequeloris66@gmail.com>
2025-04-11 13:38:46 +0200

    Pushed last changes

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -27,6 +27,6 @@ void block_destroy(block_t *block)
 	if (!block)
 		return;
 	llist_destroy(block->transactions, 1,
-		(node_dtor_t)transaction_transaction);
+		(node_dtor_t)transactions->transaction);
 	free(block);
 }
