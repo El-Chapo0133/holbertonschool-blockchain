61ac165efc0a2a11c48323322a38d2bcaaa48c90
El-Chapo0133 <levequeloris66@gmail.com>
2025-04-11 08:54:09 +0200

    Pushed last changes

---
+++ blockchain/v0.3/transaction/tx_in_create.c
@@ -0,0 +1,39 @@
+/*
+ * ==========================================================================
+ *
+ *       Filename:  tx_in_create.c
+ *
+ *    Description:  allocates and initializes a transaction input structure
+ *
+ *        Version:  1.0
+ *        Created:  11.04.2025 08:48:35
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Loris Levêque (9601), loris@leveque.website
+ *   Organization:  Holberton
+ *
+ * ==========================================================================
+ */
+
+#include "transaction.h"
+
+/*
+ * tx_in_create - allocate a new tx_in_t struct
+ * @unspent: ptr to unspent struct
+ *
+ * Return: newly created struct or NULL
+ */
+tx_in_t *tx_in_create(unspent_tx_out_t const *unspent)
+{
+	tx_in_t *txi = malloc(sizeof(tx_in_t));
+
+	if (!unspent || !txi)
+		return (NULL);
+
+	memcpy(txi->block_hash, unspent->block_hash, sizeof(unspent->block_hash));
+	memcpy(txi->tx_id, unspent->tx_id, sizeof(unspent->tx_id));
+	memcpy(txi->tx_out_hash, unspent->out.hash, sizeof(unspent->out.hash));
+	return (txi);
+}
+
