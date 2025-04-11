00d207957bd41731e86c5a6e2156ad63b9a68a0f
El-Chapo0133 <levequeloris66@gmail.com>
2025-04-11 12:02:41 +0200

    Pushed last changes

--- blockchain/v0.3/transaction/coinbase_is_valid.c
+++ blockchain/v0.3/transaction/coinbase_is_valid.c
@@ -34,6 +34,8 @@ int is_all_zero(uint8_t *buffer, size_t size)
  * coinbase_is_valid - checks if a coinbase transaction is valid
  * @coinbase: coinbase to check
  * @block_index: index of the Block the coinbase transaction will belong to
+ *
+ * Return: 1 if coinbase is valid, 0 otherwise
  */
 int coinbase_is_valid(transaction_t const *coinbase,
 		uint32_t block_index)
